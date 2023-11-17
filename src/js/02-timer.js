f2bf448dedc9bc2c0c4b82b39d8d315dacaed907
Olvet7 <volya.maslova@gmail.com>
2023-11-18 01:32:11 +0200

    2 timer

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,74 +1,90 @@
-// Імпорт бібліотеки
-import flatpickr from "flatpickr";
-import "flatpickr/dist/flatpickr.min.css";
+import flatpickr from 'flatpickr';
+import 'flatpickr/dist/flatpickr.min.css';
+import { Report } from 'notiflix/build/notiflix-report-aio';
 
-// Елементи DOM
-const calendar = document.getElementById('datetime-picker');
-const startBtn = document.querySelector('BUTTON'); 
-const daysRefs = document.querySelector['data-days'];
-const hoursRefs = document.querySelector['data-hours'];
-const minutesRefs = document.querySelector['data-minutes'];
-const secondsRefs = document.querySelector['data-seconds'];
-
-//Елементи таймеру
-let selectedDate = null;
-let currentDate = null;
-let timerId = null;
-// різниця між кінцевою і теперішньою датою в мсек
-let ms = null;
-// Відмальовка під час відліку
-let timerRedoTime = null;
-// Неактивна кнопка старт, якщо не вибрана потрібна дата
-startBtn.setAttribute('disabled', true);
-
-const options = {
-    enableTime: true,
-    time_24hr: true,
-    defaultDate: new Date(),
-    minuteIncrement: 1,
-    onClose(selectedDates) {
-      console.log(selectedDates[0].getTime);
-      deltaDates(selectedDates[0]);
-    },
-  };
-
-
-flatpickr("#datetime-picker", options);
-
-startBtn.addEventListener('click', onStartBtnDate);
-
-function onStartBtnDate(evt) {
-  timerId = setInterval(startTimer, 1000);
+// Посилання на всі елементи
+const refs = {
+  startBtn: document.querySelector('button[data-start]'),
+  days: document.querySelector('.value[data-days]'),
+  hours: document.querySelector('.value[data-hours]'),
+  minutes: document.querySelector('.value[data-minutes]'),
+  seconds: document.querySelector('.value[data-seconds]'),
 }
 
-// function startTimer() {
-//   startBtn.setAttribute('disabled', true);
-//   ms -= 1000;
+// Параметри при виборі минулої дати
+blockStartBtn()
+const options = {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose(selectedDates) {
+    const selectedDate = selectedDates[0];
+    const currentDate = new Date();
+    console.log(selectedDates[0]);
+    if (selectedDate < currentDate) {
+      blockStartBtn();
+      Report.warning(
+        'Please, choose a date in the future!'
+        );
+    } else {
+      unblockStartBtn()
+    }
+  },
+};
+
+// Підключення бібліотеки календаря
+const calendar = flatpickr('#datetime-picker', options);
+
+// Слухач на кнопку "старт"
+refs.startBtn.addEventListener('click', onStartBtn);
+
+// Функція після вибору майбутньої дати
+function onStartBtn() {
+  blockStartBtn();
+  Report.success('Timer started');
+  const timerId = setInterval(() => {
+    const futureDate = calendar.selectedDates[0];
+    const currentDate = new Date();
+    const deltaTime = futureDate - currentDate;
+    const converterDeltatime = convertMs(deltaTime);
+    refs.days.textContent = padStart(converterDeltatime.days);
+    refs.hours.textContent = padStart(converterDeltatime.hours);
+    refs.minutes.textContent = padStart(converterDeltatime.minutes);
+    refs.seconds.textContent = padStart(converterDeltatime.seconds);
+    if (deltaTime < 1000) {
+      clearInterval(timerId);
+      Report.success("Time's up");
+    }
+  }, 1000);
+}
 
 
-// }
+// Функція блокування кнопки старт
+function blockStartBtn(){
+  refs.startBtn.disabled = true;
+}
 
-// // функція конвертування проміжку часу від дефолтної дати до кінцевої
-// function convertMs(ms) {
-//     const second = 1000;
-//     const minute = second * 60;
-//     const hour = minute * 60;
-//     const day = hour * 24;
-//     const days = Math.floor(ms / day);
-//     const hours = Math.floor((ms % day) / hour);
-//     const minutes = Math.floor(((ms % day) % hour) / minute);
-//     const seconds = Math.floor((((ms % day) % hour) % minute) / second);
-//     return { days, hours, minutes, seconds };
-//   }
-  
-// // padStart - 0 перед цифрою, якщо вона <10
-// function addLeadingZero(value){
-//     return String(value).padStart(2, '0');
-// }
+// Функція розблокування кнопки старт
+function unblockStartBtn(){
+  refs.startBtn.disabled = false;
+}
 
-// function updateClockface({ days, hours, minutes, seconds }) {
-//     refs.days.textContent = '${days}';
-//     refs.hours.textContent = '${hours}';
-//     refs.minutes.textContent = '${minutes}';
-//     refs.seconds.textContent = '${seconds}';
-// }
+// Функція перерахунку мілісекунд в дні, години, хвилини, секунди
+function convertMs(ms) {
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+  const days = Math.floor(ms / day);
+  const hours = Math.floor((ms % day) / hour);
+  const minutes = Math.floor(((ms % day) % hour) / minute);
+  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+  return { days, hours, minutes, seconds };
+}
+
+// Функція додавання 0 перед цифорою, яка менша за 10
+function padStart(value){
+  return String(value).padStart(2, '0');
+}
+
